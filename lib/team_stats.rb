152aab5e04db2867ce796f4723fcafe183e43135
Reid Miller <reid.s.miller@gmail.com>
2023-03-05 11:18:09 -0700

    feat: Add best/worst season methods

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -13,4 +13,24 @@ module TeamStats
       "link" => selected_team.link
     }
   end
+
+  def best_season(input_team_id)
+    team_group = @game_teams.select {|game_team| game_team.team_id == input_team_id}
+    team_seasons = team_group.group_by(&:season)
+    team_season_percent = {}
+    team_seasons.each do |season, values|
+      team_season_percent[season] = percent_win_loss(values)
+    end
+    bestest_season = team_season_percent.max_by {|_, value| value}[0]
+  end
+
+  def worst_season(input_team_id)
+    team_group = @game_teams.select {|game_team| game_team.team_id == input_team_id}
+    team_seasons = team_group.group_by(&:season)
+    team_season_percent = {}
+    team_seasons.each do |season, values|
+      team_season_percent[season] = percent_win_loss(values)
+    end
+    bestest_season = team_season_percent.min_by {|_, value| value}[0]
+  end
 end
